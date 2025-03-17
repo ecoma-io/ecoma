afc5346eb7fc637cc0097fb96fb5653df8a37e53
John Martin <john.itvn@gmail.com>
2025-03-17 12:46:07 +0000

    chore: add devcotnainer

---
+++ .devcontainer/p10k.zsh
@@ -0,0 +1,26 @@
+# Config for Powerlevel10k with minimalism prompt style. Type `p10k configure` to generate
+# your own config based on it.
+
+'builtin' 'local' '-a' 'p10k_config_opts'
+[[ ! -o 'aliases'         ]] || p10k_config_opts+=('aliases')
+[[ ! -o 'sh_glob'         ]] || p10k_config_opts+=('sh_glob')
+[[ ! -o 'no_brace_expand' ]] || p10k_config_opts+=('no_brace_expand')
+'builtin' 'setopt' 'no_aliases' 'no_sh_glob' 'brace_expand'
+
+() {
+  emulate -L zsh -o extended_glob
+
+  unset -m '(POWERLEVEL9K_*|DEFAULT_USER)~POWERLEVEL9K_GITSTATUS_DIR'
+
+  # Zsh >= 5.1 is required.
+  [[ $ZSH_VERSION == (5.<1->*|<6->.*) ]] || return
+  typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
+  typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
+  typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last
+  typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=true
+  (( ! $+functions[p10k] )) || p10k reload
+}
+
+typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
+(( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
+'builtin' 'unset' 'p10k_config_opts'
---
+++ .devcontainer/setup.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+set -e
+
+# Define temporary directory
+TMP_DIR="/tmp/devcontainer-setup"
+mkdir -p "$TMP_DIR"
+
+
+
+# Update package list
+echo "=============================="
+echo "🚀 Updating required packages..."
+sudo apt-get update -y
+echo "✅ system is up-to-date"
+echo "Installing packages..."
+sudo apt-get install -y curl git jq
+echo "✅ common tools installed"
+
+
+# Install kubectl
+echo "=============================="
+echo "🚀 Installing kubectl..."
+if [ -f /usr/local/bin/kubectl ]; then
+    echo "✅ kubectl is already installed"
+else   
+    KUBECTL_BIN="$TMP_DIR/kubectl"
+    curl -Lo "$KUBECTL_BIN" "https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x "$KUBECTL_BIN"
+    sudo mv "$KUBECTL_BIN" /usr/local/bin/
+    echo "✅ kubectl installed"
+fi
+
+# Install kubeseal
+echo "=============================="
+echo "🚀 Installing kubeseal..."
+if [ -f /usr/local/bin/kubeseal ]; then
+    echo "✅ kubeseal is already installed"
+else
+    echo "✅ finding the latest kubeseal version..."
+    KUBESEAL_VERSION=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/tags | jq -r '.[0].name' | cut -c 2-)
+    if [ -z "$KUBESEAL_VERSION" ]; then
+        echo "Failed to fetch the latest KUBESEAL_VERSION"
+        exit 1
+    fi
+    echo "Downloading kubeseal version: $KUBESEAL_VERSION"
+    KUBESEAL_TAR="$TMP_DIR/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
+    curl -Lo "$KUBESEAL_TAR" "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
+    tar -xvzf "$KUBESEAL_TAR" -C "$TMP_DIR" kubeseal
+
+    echo "Installing kubeseal..."
+    sudo install -m 755 "$TMP_DIR/kubeseal" /usr/local/bin/kubeseal
+    echo "✅ kubeseal installed"
+fi
+
+# Install k3d
+echo "=============================="
+echo "🚀 Installing k3d..."
+if [ -f /usr/local/bin/k3d ]; then
+    echo "✅ k3d is already installed"
+else
+    curl -L https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+    echo "✅ k3d installed"
+fi
+
+
+# Install helm
+echo "=============================="
+echo "🚀 Installing helm..."
+if [ -f /usr/local/bin/helm ]; then
+    echo "✅ helm is already installed"
+else
+    curl -fL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    echo "✅ helm installed"
+fi
+
+# Install zsh-autosuggestions
+echo "=============================="
+echo "🚀 Installing zsh plugins..."
+ZSH_AUTOSUGGESTIONS_DIR="$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+if [ ! -d "$ZSH_AUTOSUGGESTIONS_DIR" ]; then
+    echo "Cloning zsh-autosuggestions..."
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_AUTOSUGGESTIONS_DIR"
+fi
+ZSHRC="$HOME/.zshrc"
+sed -i "s/plugins=(.*)/plugins=(git git-auto-fetch zsh-autosuggestions)/g" "$ZSHRC"
+
+
+echo "=============================="
+echo "🚀 Installing zsh theme..."
+POWERLEVEL10K="$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+if [ ! -d "$POWERLEVEL10K" ]; then
+    THEME_VERSION=$(curl --silent "https://api.github.com/repos/romkatv/powerlevel10k/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+    curl -L https://github.com/romkatv/powerlevel10k/archive/refs/tags/v"${THEME_VERSION}".zip -o /tmp/powerlevel10k.zip
+    unzip /tmp/powerlevel10k.zip -d /tmp
+    cp -r /tmp/powerlevel10k-"${THEME_VERSION}" "$POWERLEVEL10K"
+fi
+
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' "${HOME}/.zshrc"
+grep -qxF 'source ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme' "${HOME}/.zshrc" || echo 'source ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme' >> "${HOME}/.zshrc"
+grep -qxF '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' "${HOME}/.zshrc" || echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >>"${HOME}/.zshrc"
+cp ".devcontainer/p10k.zsh" "${HOME}/.p10k.zsh"    
+
+
+# Add binary paths to bashrc
+LINE="export PATH=$(pwd)/scripts:\$PATH"
+FILE="$HOME/.zshrc"
+grep -qxF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+sudo chmod +x scripts/*
+
+echo "🎉 Setup completed successfully!"
